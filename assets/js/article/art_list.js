f86797e5cd18ab9f2bec9cc18e89a1fff5b1709f
z <123@qq.com>
2020-09-15 10:38:57 +0800

    分页功能

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,8 +1,10 @@
 $(function() {
     var layer = layui.layer
     var form = layui.form
-        // 定义一个查询的参数对象，将来请求数据的时候，
-        // 需要将请求参数对象提交到服务器
+    var laypage = layui.laypage
+
+    // 定义一个查询的参数对象，将来请求数据的时候，
+    // 需要将请求参数对象提交到服务器
     var q = {
         pagenum: 1, // 页码值，默认请求第一页的数据
         pagesize: 2, // 每页显示几条数据，默认每页显示2条
@@ -44,6 +46,7 @@ $(function() {
                 }
                 var temp = template('tpl-table', res)
                 $('tbody').html(temp)
+                renderPage(res.total)
             }
 
         })
@@ -82,7 +85,44 @@ $(function() {
         initTable()
     })
 
+    // 分页
+    function renderPage(total) {
+        // 调用 laypage.render() 方法来渲染分页的结构
+        laypage.render({
+            elem: 'pageBox', // 分页容器的 Id
+            count: total, // 总数据条数
+            limit: q.pagesize, // 每页显示几条数据
+            curr: q.pagenum, // 设置默认被选中的分页
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
 
+            // 分页发生切换的时候，触发 jump 回调
+            // 触发 jump 回调的方式有两种：
+            // 1. 点击页码的时候，会触发 jump 回调
+            // 2. 只要调用了 laypage.render() 方法，就会触发 jump 回调
+
+            jump: function(obj, first) {
+                // 可以通过 first 的值，来判断是通过哪种方式，触发的 jump 回调
+                // 如果 first 的值为 true，证明是方式2触发的
+                // 否则就是方式1触发的
+                // console.log(first);
+                // console.log(obj.curr); //得到当前页
+                // 把最新的页码值，赋值到 q 这个查询参数对象中
+
+                q.pagenum = obj.curr
+                q.pagesize = obj.limit
+                    // 根据最新的 q 获取对应的数据列表，并渲染表格
+                    // initTable()
+                    // 第一次的时候不执行（会造成死循环），当点击页码的时候重新调用initTable渲染当前页的数据
+
+                if (!first) {
+                    initTable()
+                }
+            }
+        })
+
+
+    }
 
 
 
